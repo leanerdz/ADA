cbf847be9cb183ce46f4ab7c7a667637124b09df
leanerdz <leane.rodriguez3@gmail.com>
2024-02-13 23:07:30 +0100

    updates

--- sketch.js
+++ sketch.js
@@ -14,15 +14,15 @@ let world1TileSize = 64
 
 let world2TileDictionnary = {}
 let world2Board = [
-  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [0,12,0,12,12,12,12,12,12,12,12,12,12,12,12,12,12,0],
-  [0,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,0],
-  [0,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,0],
-  [0,12,12,12,12,12,12,12,12,12,12,12,12,1,12,12,12,0],
-  [0,12,12,1,1,12,12,12,12,12,12,12,1,1,12,12,12,0],
-  [0,12,1,1,1,1,12,12,12,1,1,1,1,1,1,1,1,0],
-  [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
+  [6,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,0],
+  [6,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,0],
+  [6,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,0],
+  [6,2,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,0],
+  [6,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,0],
+  [6,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,0],
+  [6,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,0],
+  [6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5],
 ]
 let world2TileSize = 64
 let currentWorld = 0
@@ -40,28 +40,31 @@ let heroY = 6*world1TileSize
 function setup() {
   createCanvas(world1Board[0].length*world1TileSize,world1Board.length*world1TileSize);
   world1TileDictionnary = { 
+                    0:loadImage('assets/arbres.png'),
+                    4:loadImage('assets/arbres_haut.png'),
+                    5:loadImage('assets/arbres_bas.png'),
+                    6:loadImage('assets/arbres_cote.png'),
+                    12:loadImage('assets/sol.png'),
+                    1:loadImage('assets/herbe.jpg'),
+                    2:loadImage('assets/House.png'),
+                    3:loadImage('assets/lune.jpg')
+                    }
+
+  world2TileDictionnary = { 
                     0: loadImage('assets/arbres.png'),
                     4: loadImage('assets/arbres_haut.png'),
                     5: loadImage('assets/arbres_bas.png'),
                     6: loadImage('assets/arbres_cote.png'),
-                     12:loadImage('assets/sol.png'),
-                     1:loadImage('assets/herbe.jpg'),
-                     2:loadImage('assets/House.png'),
-                     3:loadImage('assets/lune.jpg')
-                    }
-
-  world2TileDictionnary = { 
-                      0: loadImage('assets/bord.jpg'),
-                       12:loadImage('assets/ciel.jpg'),
-                       1:loadImage('assets/herbe.jpg'),
-                       2:loadImage('assets/pierre.jpg'),
-                       3:loadImage('assets/lune.jpg')
-                      }
+                    12:loadImage(''),
+                    1:loadImage('assets/herbe.jpg'),
+                    2:loadImage('assets/House.png'),
+                    3:loadImage('assets/lune.jpg')
+    }
 
   hero = loadImage('assets/hero2.png');
-  worlds = [world1Board,world2Board]
-  tileDictionnaries = [world1TileDictionnary,world2TileDictionnary]
-  worldsTileSizes = [world1TileSize,world2TileSize]
+  worlds = [world1Board]
+  tileDictionnaries = [world1TileDictionnary]
+  worldsTileSizes = [world1TileSize]
 }
 
 function drawWorld(gameBoard,tileDictionnary,tileSize) {
@@ -70,7 +73,17 @@ function drawWorld(gameBoard,tileDictionnary,tileSize) {
     for (let x = 0; x < currentLine.length; x++) {
       const currentTileValue = currentLine[x];
       let currentImageName = tileDictionnary[currentTileValue];
-      // Dessiner l'image
+      image(currentImageName, x*tileSize, y*tileSize, tileSize, tileSize);
+    }
+  }
+}
+
+function drawFront(gameBoard,tileDictionnary,tileSize) {
+  for (let y = 0; y < gameBoard.length; y++) {
+    const currentLine = gameBoard[y];
+    for (let x = 0; x < currentLine.length; x++) {
+      const currentTileValue = currentLine[x];
+      let currentImageName = tileDictionnary[currentTileValue];
       image(currentImageName, x*tileSize, y*tileSize, tileSize, tileSize);
     }
   }
@@ -84,7 +97,7 @@ function checkCollision(gameBoard,tileSize) {
       const currentTileValue = currentLine[x];    
       console.log(currentTileValue)  
       if (currentTileValue === 0 || currentTileValue === 1 || currentTileValue === 2 || currentTileValue === 4|| currentTileValue === 5|| currentTileValue === 6){
-        if(rectIsInRect(x*tileSize-tileSize, y*tileSize-tileSize, heroX, heroY, tileSize*2, tileSize*2)){
+        if(rectIsInRect(x*tileSize-tileSize, y*tileSize-tileSize, heroX, heroY, heroWidth,heroHeight)){
           return true 
         }
       }
@@ -135,11 +148,13 @@ function keyPressed() {
 
 // Appelé en continue après le setup
 function draw() {
-
-  drawWorld(worlds[currentWorld],
-            tileDictionnaries[currentWorld],
-            worldsTileSizes[currentWorld]);
-  
+  drawWorld(worlds[currentWorld], tileDictionnaries[currentWorld], worldsTileSizes[currentWorld]);
   image(hero, heroX, heroY, heroWidth, heroHeight);
-  //background(20);
+  drawFront(world2Board, world2TileDictionnary, world2TileSize);
+
 }
+
+// Choix graphiques finaux 
+// Wireframes 
+//logo chartre graphique 
+// Passer perso derrière qqch, tete pas bloqué par la maison, point and click (menu, taverne), créer deux mondes, prototypes de menu, monde de dessus, monde point and click. Intégrer les sprites des disigners. 
