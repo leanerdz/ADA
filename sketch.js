d2127f58bdc86f05c55db3db7c000aa970d8e142
leanerdz <leane.rodriguez3@gmail.com>
2024-05-23 14:48:35 +0200

    taille menu

--- sketch.js
+++ sketch.js
@@ -2204,7 +2204,7 @@ function draw() {
     }
     image(menu[currentMenu], 22, 22);
     if (showMenu) {
-      image(menuComplet,0,0, 1280,720);
+      image(menuComplet,0,0, 1280,768);
       if (menuClickCounter === 2){
         showMenu = false;
         menuClickCounter = 0;
